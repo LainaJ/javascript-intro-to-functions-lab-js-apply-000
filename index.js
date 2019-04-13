0d688f6719ef45cd8ad055f10a8f2cffa7d24d69
Laina Karosic <lainakarosic@gmail.com>
2019-04-13 22:50:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ console.log(string.toLowerCase());
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string){
   return "I can't hear you!"}
-  else {"YES INDEED!"
+  else if (string.toUpperCase() === string){
+  return "YES INDEED!"}
   }
-}
